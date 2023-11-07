455ae27319e09419b5e31aa4c839eb3b4703c7ed
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-11-07 19:02:59 +0700

    First Commit

---
+++ app/src/main/java/com/example/esteh/data/SumberData.kt
@@ -0,0 +1,15 @@
+package com.example.esteh.data
+
+import com.example.esteh.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+
+
+    )
+}
